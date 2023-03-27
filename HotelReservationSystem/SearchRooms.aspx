6b2f8d68581dfdb73d6d54e274ec59f23f880a56
_ <128780634+HatataMakuna@users.noreply.github.com>
2023-03-27 23:44:21 +0800

    Add SearchRooms
    button and search results not worked yet

---
+++ HotelReservationSystem/SearchRooms.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="SearchRooms.aspx.cs" Inherits="Assignment.SearchRooms" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Search Your Desired Room</h2>
+    <asp:TextBox ID="LocationText" runat="server" placeholder="Location"></asp:TextBox>
+    <asp:TextBox ID="NoRoomsText" runat="server" placeholder="Number of rooms"></asp:TextBox>
+    <asp:TextBox ID="NoAdultsText" runat="server" placeholder="Number of adults"></asp:TextBox>
+    <asp:TextBox ID="NoChildrenText" runat="server" placeholder="Number of children"></asp:TextBox>
+    <asp:TextBox ID="MinPriceText" runat="server" placeholder="Minimum price"></asp:TextBox>
+    <asp:TextBox ID="MaxPriceText" runat="server" placeholder="Maximum price"></asp:TextBox>
+    <br />
+
+    <br />
+    <asp:Button ID="btnSearch" runat="server" Text="Button" OnClick="btnSearch_Click" />
+    <br />
+    <div>
+        <!-- Retrieve the rooms database
+            SELECT * FROM [room]
+            WHERE [blah] = %LIKE% smth
+            -->
+        <%--<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" DataSourceID="SqlDataSource1">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="True" SortExpression="Id" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                <asp:BoundField DataField="Location" HeaderText="Location" SortExpression="Location" />
+                <asp:BoundField DataField="NumRooms" HeaderText="NumRooms" SortExpression="NumRooms" />
+                <asp:BoundField DataField="NumAdults" HeaderText="NumAdults" SortExpression="NumAdults" />
+                <asp:BoundField DataField="NumChildren" HeaderText="NumChildren" SortExpression="NumChildren" />
+                <asp:BoundField DataField="Price" HeaderText="Price" SortExpression="Price" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server"
+            ConnectionString="<%$ ConnectionStrings:ConnectionString %>"
+            SelectCommand="SELECT * FROM [Room]"></asp:SqlDataSource>--%>
+
+    </div>
+</asp:Content>
---
+++ HotelReservationSystem/SearchRooms.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class SearchRooms : System.Web.UI.Page
+    {
+        //private List<Hotel> SearchHotels(string location, DateTime checkin, DateTime checkout, int numRooms, int numAdults, int numChildren)
+        //{
+        //    // Perform the search based on the criteria and return a list of hotels
+        //    List<Hotel> hotels = new List<Hotel>();
+
+        //    // Example code for searching hotels
+        //    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        //    con.Open();
+
+        //    SqlCommand cmd = new SqlCommand("SELECT * FROM Hotels WHERE Location=@Location AND NumRooms>=@NumRooms AND NumAdults>=@NumAdults AND NumChildren>=@NumChildren", con);
+        //    cmd.Parameters.AddWithValue("@Location", location);
+        //    cmd.Parameters.AddWithValue("@NumRooms", numRooms);
+        //    cmd.Parameters.AddWithValue("@NumAdults", numAdults);
+        //    cmd.Parameters.AddWithValue("@NumChildren", numChildren);
+
+        //    SqlDataReader dr = cmd.ExecuteReader();
+
+        //    while (dr.Read())
+        //    {
+        //        Hotel hotel = new Hotel();
+        //        hotel.Id = int.Parse(dr["Id"].ToString());
+        //        hotel.Name = dr["Name"].ToString();
+        //        hotel.Location = dr["Location"].ToString();
+        //        hotel.NumRooms = int.Parse(dr["NumRooms"].ToString());
+        //        hotel.NumAdults = int.Parse(dr["NumAdults"].ToString());
+        //        hotel.NumChildren = int.Parse(dr["NumChildren"].ToString());
+        //        hotel.Price = decimal.Parse(dr["Price"].ToString());
+        //        hotels.Add(hotel);
+        //    }
+
+        //    con.Close();
+
+        //    return hotels;
+        //}
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Get the search criteria entered by the user
+            /*
+            string location = txtLocation.Text;
+            DateTime checkin = DateTime.Parse(txtCheckin.Text);
+            DateTime checkout = DateTime.Parse(txtCheckout.Text);
+            int numRooms = int.Parse(ddlNumRooms.SelectedValue);
+            int numAdults = int.Parse(ddlNumAdults.SelectedValue);
+            int numChildren = int.Parse(ddlNumChildren.SelectedValue);
+            */
+
+            // Call a method to search for hotels based on the criteria
+            //List<Hotel> hotels = SearchHotels(location, checkin, checkout, numRooms, numAdults, numChildren);
+
+            // Bind the search results to a GridView or Repeater control
+            //gvSearchResults.DataSource = hotels;
+            //gvSearchResults.DataBind();
+        }
+    }
+}
