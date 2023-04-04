8b7aca539c4c40024dcfb12f2717b988c127889d
ii887522 <ii887522@gmail.com>
2023-04-05 00:50:35 +0800

    Add logout option in header

--- HotelReservationSystem/Site1.Master.cs
+++ HotelReservationSystem/Site1.Master.cs
@@ -7,11 +7,20 @@ using System.Web.UI.WebControls;
 
 namespace Assignment
 {
-    public partial class Site1 : System.Web.UI.MasterPage
+  public partial class Site1 : System.Web.UI.MasterPage
+  {
+    protected void Page_Load(object sender, EventArgs e)
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
 
-        }
     }
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+      var remember_me_cookie = new HttpCookie("remember_me", "1");
+      remember_me_cookie.Expires = DateTime.Now.AddDays(-1);
+      Response.SetCookie(remember_me_cookie);
+      Session["IsLoggedIn"] = false;
+      Response.Redirect(HttpContext.Current.Request.Url.AbsolutePath);
+    }
+  }
 }
