38c8dda61ba05f176bf52e72c10ebfe794430125
ii887522 <ii887522@gmail.com>
2023-04-05 10:09:53 +0800

    Reduce hardcoding string

---
+++ HotelReservationSystem/Constants.cs
@@ -0,0 +1,14 @@
+namespace HotelReservationSystem
+{
+  public class Constants
+  {
+    // Database names
+    public const string LocalSqlServer = "LocalSqlServer";
+
+    // Cookie keys
+    public const string RememberMe = "remember_me";
+
+    // Session keys
+    public const string IsLoggedIn = "IsLoggedIn";
+  }
+}
